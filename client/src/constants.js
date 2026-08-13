1f05aea5922ce06fd42a31f6c087053910f2a16b
neelam <ny7726945@gmail.com>
2026-08-13 15:30:32 +0530

    Shorten Talent Acquisition and Marketing certificate type labels

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,8 +1,8 @@
 export const CERT_TYPES = {
-  ta_1: "Talent Acquisition (1 Month)",
-  ta_2: "Talent Acquisition (2 Month)",
-  ta_4: "Talent Acquisition (4 Month)",
-  ta_6: "Talent Acquisition (6 Month)",
+  ta_1: "TA 1 Month",
+  ta_2: "TA 2 Month",
+  ta_4: "TA 4 Month",
+  ta_6: "TA 6 Month",
   ex: "Excellence",
   ap: "Appreciation",
   ca: "Campus Ambassador",
@@ -11,6 +11,6 @@ export const CERT_TYPES = {
   fsd_2m: "Full Stack Developer (2 Month)",
   ma_2m: "Marketing (2 Month)",
   bda_2m: "Business Development Associate (2 Month)",
-  tasm_2: "Talent Acquisition & Marketing (2 Month)",
-  tasm_4: "Talent Acquisition & Marketing (4 Month)"
+  tasm_2: "TASM 2 Month",
+  tasm_4: "TASM 4 Month"
 };
--- server/src/constants/certTypes.js
+++ server/src/constants/certTypes.js
@@ -1,8 +1,8 @@
 const CERT_TYPES = {
-  ta_1: "Talent Acquisition (1 Month)",
-  ta_2: "Talent Acquisition (2 Month)",
-  ta_4: "Talent Acquisition (4 Month)",
-  ta_6: "Talent Acquisition (6 Month)",
+  ta_1: "TA 1 Month",
+  ta_2: "TA 2 Month",
+  ta_4: "TA 4 Month",
+  ta_6: "TA 6 Month",
   ex: "Excellence",
   ap: "Appreciation",
   ca: "Campus Ambassador",
@@ -11,8 +11,8 @@ const CERT_TYPES = {
   fsd_2m: "Full Stack Developer (2 Month)",
   ma_2m: "Marketing (2 Month)",
   bda_2m: "Business Development Associate (2 Month)",
-  tasm_2: "Talent Acquisition & Marketing (2 Month)",
-  tasm_4: "Talent Acquisition & Marketing (4 Month)"
+  tasm_2: "TASM 2 Month",
+  tasm_4: "TASM 4 Month"
 };
 
 const CERT_TYPE_KEYS = Object.keys(CERT_TYPES);
